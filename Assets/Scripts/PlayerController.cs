8cd5c245f73326de478a94e765dc0744099f791e
afoolwithzeroeq <2752583070@qq.com>
2020-11-16 17:43:09 +0800

    添加场景，并和骰子部分合并。
    场景实现了简要的游戏框架和UI，包括移动等。其中WASD实现移动，鼠标和QE左右转向，G切换鼠标。

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Transform m_Transform;
+
+    private bool mouseLock = true;
+
+    void Start()
+    {
+
+        m_Transform = gameObject.GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+        MoveControl();
+        Cursor.visible = !mouseLock;
+        if (mouseLock)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            Debug.Log("aaa");
+        }
+    }
+
+    void MoveControl()
+
+    {
+
+        if (Input.GetKey(KeyCode.W))
+
+        {
+
+            m_Transform.Translate(Vector3.forward * 0.1f, Space.Self);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+
+        {
+
+            m_Transform.Translate(Vector3.back * 0.1f, Space.Self);
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+
+        {
+
+            m_Transform.Translate(Vector3.left * 0.1f, Space.Self);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+
+        {
+
+            m_Transform.Translate(Vector3.right * 0.1f, Space.Self);
+
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+
+        {
+
+            m_Transform.Rotate(Vector3.up, -1.0f);
+
+        }
+
+        if (Input.GetKey(KeyCode.E))
+
+        {
+
+            m_Transform.Rotate(Vector3.up, 1.0f);
+
+        }
+
+        if (Input.GetKey(KeyCode.G))
+
+        {
+
+            mouseLock = !mouseLock;
+
+        }
+
+        if (mouseLock)
+        {
+            m_Transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
+        }
+
+        //m_Transform.Rotate(Vector3.left, Input.GetAxis("Mouse Y"));
+
+    }
+
+}
---
+++ Assets/Scripts/UIdemo.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIdemo : MonoBehaviour
+{
+    struct Player
+    {
+        public int Money;
+
+        public Player(int money)
+        {
+            Money = money;
+        }
+    }
+
+    const int StartMoney = 20000;
+    private int GameTurn;
+
+    Player player1 = new Player(StartMoney), player2 = new Player(StartMoney), player3 = new Player(StartMoney), player4 = new Player(StartMoney);
+
+    public GameObject PlayerName;
+    public GameObject MoneyRank1, MoneyRank2, MoneyRank3, MoneyRank4;
+    public GameObject DiceNum;
+    public GameObject Capsule;
+
+    public float rotateSpeed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameTurn = 1;
+        Application.targetFrameRate = 120;
+        PlayerName.GetComponent<Text>().text = "Player1";
+        MoneyRank1.GetComponent<Text>().text = StartMoney.ToString();
+        MoneyRank2.GetComponent<Text>().text = StartMoney.ToString();
+        MoneyRank3.GetComponent<Text>().text = StartMoney.ToString();
+        MoneyRank4.GetComponent<Text>().text = StartMoney.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Capsule.transform.Rotate(new Vector3(Time.deltaTime * rotateSpeed, Time.deltaTime * rotateSpeed, Time.deltaTime * rotateSpeed));
+        if (player1.Money <= 0)
+        {
+
+        }
+    }
+
+    public void EndButtonOnClick()
+    {
+        Application.Quit();
+    }
+
+    public void DiceButtonOnClicked()
+    {
+        int dice = Random.Range(1, 10);
+        DiceNum.GetComponent<Text>().text = dice.ToString();
+        switch (GameTurn)
+        {
+            case 1:
+                player1.Money -= dice * 1000;
+                MoneyRank1.GetComponent<Text>().text = player1.Money.ToString();
+                PlayerName.GetComponent<Text>().text = "Player2";
+                break;
+            case 2:
+                player2.Money -= dice * 1000;
+                MoneyRank2.GetComponent<Text>().text = player2.Money.ToString();
+                PlayerName.GetComponent<Text>().text = "Player3";
+                break;
+            case 3:
+                player3.Money -= dice * 1000;
+                MoneyRank3.GetComponent<Text>().text = player3.Money.ToString();
+                PlayerName.GetComponent<Text>().text = "Player4";
+                break;
+            case 4:
+                player4.Money -= dice * 1000;
+                MoneyRank4.GetComponent<Text>().text = player4.Money.ToString();
+                PlayerName.GetComponent<Text>().text = "Player1";
+                break;
+            default:break;
+        }
+        GameTurn++;
+        if(GameTurn == 5)
+        {
+            GameTurn = 1;
+        }
+        
+    }
+}
