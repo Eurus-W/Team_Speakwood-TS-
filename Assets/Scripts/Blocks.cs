40a80e6cf32b53ce5c0cca81095b5d7f0618d212
HSC-stu <2436181568@qq.com>
2020-12-05 10:54:29 +0800

    添加地块类和监听脚本
    实现对过路费的动态改变

---
+++ Assets/Scripts/Blocks.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public class Block
+{
+    public GameObject blockModel;
+
+    public int price;
+    public string type;
+    public int owner ;
+    public int house ;
+    public int color;
+    public int rent;
+    public Block(int i ,GameObject obj)
+    {
+        blockModel = obj;
+        if (i == 4 || i == 12 || i == 22 || i == 30)
+        {
+            price = 1200;
+            type = "port";
+            rent = 240;
+        }
+        else
+        {
+            switch (i)
+            {
+                case 0: price = 0; type = "start"; break;
+                case 1: price = 600; type = "blocks"; color = 1; rent = 120; owner = 0; break;
+                case 2: price = 720; type = "blocks"; color = 1; rent = 144; owner = 0; break;
+                case 3: price = 800; type = "blocks"; color = 1; rent = 160; owner = 0; break;
+                case 5: price = 930; type = "blocks"; color = 2; rent = 186; owner = 0; break;
+                case 6: price = 0; type = "chance"; break;
+                case 7: price = 1000; type = "blocks"; color = 2; rent = 200; owner = 0; break;
+                case 8: price = 1070; type = "blocks"; color = 2; rent = 214; owner = 0; break;
+                case 9: price = 0; type = "hospital"; break;
+                case 10: price = 1130; type = "blocks"; color = 3; rent = 226; owner = 0; break;
+                case 11: price = 1200; type = "blocks"; color = 3; rent = 240; owner = 0; break;
+                case 13: price = 1300; type = "blocks"; color = 3; rent = 260; owner = 0; break;
+                case 14: price = 2500; type = "special"; rent = 500; owner = 0; break;
+                case 15: price = 1410; type = "blocks"; color = 4; rent = 282; owner = 0; break;
+                case 16: price = 1500; type = "blocks"; color = 4; rent = 300; owner = 0; break;
+                case 17: price = 1600; type = "blocks"; color = 4; rent = 320; owner = 0; break;
+                case 18: price = 0; type = "store"; break;
+                case 19: price = 1630; type = "blocks"; color = 5; rent = 326; owner = 0; break;
+                case 20: price = 1690; type = "blocks"; color = 5; rent = 338; owner = 0; break;
+                case 21: price = 1700; type = "blocks"; color = 5; rent = 340; owner = 0; break;
+                case 23: price = 1830; type = "blocks"; color = 6; rent = 366; owner = 0; break;
+                case 24: price = 0; type = "fate"; break;
+                case 25: price = 1960; type = "blocks"; color = 6; rent = 392; owner = 0; break;
+                case 26: price = 2000; type = "blocks"; color = 6; rent = 400; owner = 0; break;
+                case 27: price = 0; type = "hospital"; break;
+                case 28: price = 2210; type = "blocks"; color = 7; rent = 442; owner = 0; break;
+                case 29: price = 2300; type = "blocks"; color = 7; rent = 460; owner = 0; break;
+                case 30: price = 2500; type = "special"; rent = 500; owner = 0; break;
+                case 31: price = 2310; type = "blocks"; color = 7; rent = 462; owner = 0; break;
+                case 33: price = 1930; type = "blocks"; color = 8; rent = 386; owner = 0; break;
+                case 34: price = 1800; type = "blocks"; color = 8; rent = 360; owner = 0; break;
+                case 35: price = 1700; type = "blocks"; color = 8; rent = 340; owner = 0; break;
+
+            }
+        }
+        
+    }
+    
+    public void RecoverRent()
+    {
+        rent = (int)((house + 1) * price * 0.2);
+    }
+    public void DoubleRent()
+    {       
+        rent = (int)((house + 1) * price * 0.4);
+    }
+    public void TribleRent()
+    {
+        rent = (int)((house + 1) * price * 0.6);
+    }
+    public int GetRent()
+    {
+        return rent;
+    }
+
+}
+public class Blocks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        
+        
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/BlocksChange.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Block[] all_blocks;
+    void Start()
+    {
+        
+        GameObject obj= GameObject.Find("Cube");
+        all_blocks = new Block[36];
+        Block b;
+        for (int i =0;i<=35;i++)
+        {
+            b=new Block(i,obj);
+            all_blocks[i] = b;
+        }
+        Debug.Log(all_blocks[0].price);
+        
+        
+    }
+    public void DoubleGroundRent(Block[] all_blocks, int b1, int b2, int b3)
+    {
+        if (all_blocks[b1].owner == all_blocks[b2].owner && all_blocks[b1].owner == all_blocks[b3].owner && all_blocks[b1].owner != 0)
+        {
+            all_blocks[b1].TribleRent();
+            all_blocks[b2].TribleRent();
+            all_blocks[b3].TribleRent();
+        }
+        else if (all_blocks [b1].owner !=all_blocks [b2].owner && all_blocks [b1].owner != all_blocks [b3].owner && all_blocks [b2].owner != all_blocks [b3].owner )
+        {
+            all_blocks[b1].DoubleRent();
+            all_blocks[b2].DoubleRent();
+            all_blocks[b3].DoubleRent();
+
+        }
+        else
+        {
+            all_blocks[b1].RecoverRent();
+            all_blocks[b2].RecoverRent();
+            all_blocks[b3].RecoverRent();
+        }
+        
+            
+
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        DoubleGroundRent(all_blocks, 1, 2, 3);
+        DoubleGroundRent(all_blocks, 5, 7, 8);
+        DoubleGroundRent(all_blocks, 10, 11, 13);
+        DoubleGroundRent(all_blocks, 15, 16, 17);
+        DoubleGroundRent(all_blocks, 19, 20, 21);
+        DoubleGroundRent(all_blocks, 23, 25, 26);
+        DoubleGroundRent(all_blocks, 28, 29, 31);
+        DoubleGroundRent(all_blocks, 33, 34, 35);
+        all_blocks[14].RecoverRent();
+        all_blocks[30].RecoverRent();
+        Dictionary<int, int> port=new Dictionary<int, int> ();
+        port[all_blocks[4].owner] = 0;
+        port[all_blocks[12].owner] = 0;
+        port[all_blocks[22].owner] = 0;
+        port[all_blocks[30].owner] = 0;
+        port[all_blocks[4].owner] += 1;
+        port[all_blocks[12].owner] += 1;
+        port[all_blocks[22].owner] += 1;
+        port[all_blocks[30].owner] += 1;
+
+        foreach (KeyValuePair<int,int> kvp in port)
+        {
+            if (all_blocks[4].owner == kvp.Key) { all_blocks[4].rent = 240 + (kvp.Value - 1) * 500; }
+            if (all_blocks[12].owner == kvp.Key) { all_blocks[12].rent = 240 + (kvp.Value - 1) * 500; }
+            if (all_blocks[22].owner == kvp.Key) { all_blocks[22].rent = 240 + (kvp.Value - 1) * 500; }
+            if (all_blocks[30].owner == kvp.Key) { all_blocks[30].rent = 240 + (kvp.Value - 1) * 500; }
+        }
+        
+    }
+}
