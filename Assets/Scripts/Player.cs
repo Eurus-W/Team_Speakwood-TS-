394c207bfc5b9674fcf5d9a794517a95c0dc5530
afoolwithzeroeq <2752583070@qq.com>
2020-12-10 22:09:35 +0800

    Add StartUI（开始界面）和优化UIdemo（游戏界面）
    1.实现了开始界面点击本地游戏后切换到游戏
    2.实现了游戏界面显示部分具体信息和Icon，包括武德、力敏智幸体、heroInfo、skill等。

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player
 {
+    //heros id
+    private int key;
+    
     //持有金钱数
     private int money;
     //绑定场景中的物体
@@ -39,7 +42,7 @@ public class Player
         //根据不同的英雄id对玩家进行初始化
 
         if (heroid == -1)//当前测试阶段的的default初始化
-        {
+        { 
             money = 20000;
             Strength = 30;
             agility = 30;
@@ -62,13 +65,15 @@ public class Player
         }
         else
         {
+            Heroes data = GameObject.Find("HeroData").GetComponent<ReadData>().LoadedData;
             for (int i = 0; i < GameObject.Find("HeroData").GetComponent<ReadData>().LoadedData.heroes.Length; i++)
             {
-                Heroes data = GameObject.Find("HeroData").GetComponent<ReadData>().LoadedData;
                 if (heroid == data.heroes[i].Heroid)
                 {
+                    key = i;
+                    
                     //money = data.heroes[i];
-                    money = 30000;
+                    money = GameObject.Find("Player").GetComponent<UIdemo>().StartMoney;
                     strength = data.heroes[i].Strength;
                     agility = data.heroes[i].Agility;
                     intelligence = data.heroes[i].Intelligence;
@@ -113,6 +118,7 @@ public class Player
     public bool IsMoving { get => isMoving; set => isMoving = value; }
     public bool HaveMoved { get => haveMoved; set => haveMoved = value; }
     public int Position { get => position; set => position = value; }
+    public int Key { get => key; set => key = value; }
 
     //构造函数
 
